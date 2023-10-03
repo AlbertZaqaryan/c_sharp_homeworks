83ba58e27f96cc4b9755b882713e9dd8f3832132
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2023-10-03 18:15:07 +0400

    c#_v1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// -------------------------------------------------------------
+// string path = "english.txt";
+// List<char> mylist = new List<char> {'a', 'e', 'i', 'o', 'u', 'y'};
+// string text = File.ReadAllText(path);
+// string[] arr = text.Split(" ");
+// foreach(string i in arr){
+//     int m = 0;
+//     int n = 0;
+//     while(true){
+//         if(i[m] != mylist[n]){
+//             m ++;
+//         }
+//         else{
+//             n++;}
+//         if(n == 6){
+//             System.Console.WriteLine(i);
+//             break;
+//         }
+//         else if(m == i.Length && n != 6) break;
+//     }
+// }
+// -------------------------------------------------------------
+
